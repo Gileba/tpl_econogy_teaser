a00b3060115f189a6533ed2cf394eca49a3ed3e8
Gijs Lamon <info@gileba.be>
2021-02-02 11:11:39 +0100

    Update index.php
    Google Tag Manager

--- src/index.php
+++ src/index.php
@@ -23,11 +23,29 @@
    xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
    <head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Google Tag Manager -->
+		<script>
+			(function(w,d,s,l,i){
+				w[l]=w[l]||[];
+				w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+				var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+				j.async=true;
+				j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+				f.parentNode.insertBefore(j,f);
+			})
+			(window,document,'script','dataLayer','GTM-NLF5NX7');
+		</script>
+<!-- End Google Tag Manager -->
 		<jdoc:include type="head" />
 		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css" type="text/css" />
 		<link rel="stylesheet" href="https://use.typekit.net/wbb6jqc.css">
 	</head>
 	<body class="<?php echo $pageclass ? htmlspecialchars($pageclass) : 'default'; ?>">
+<!-- Google Tag Manager (noscript) -->
+		<noscript>
+			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NLF5NX7" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+		</noscript>
+<!-- End Google Tag Manager (noscript) -->
 		<div class="container">
 			<div class="header">
 				<jdoc:include type="modules" name="header" />
